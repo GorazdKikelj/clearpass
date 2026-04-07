31f8e223984cfff8e83c916ac60daa557edd6ed8
Gorazd Kikelj <gorazd.kikelj@gmail.com>
2026-04-07 16:43:26 +0200

    refined sql output. updated device_category matching.

--- endpoint2guest.sql
+++ endpoint2guest.sql
@@ -8,14 +8,17 @@ SELECT
         substring(mac_address, 9,2) || '-' || 
         substring(mac_address, 11,2)) as mac,
   CASE WHEN (hostname IS NOT NULL AND hostname != '') THEN hostname ELSE mac_address END AS visitor_name,
-   CASE 
-    WHEN lower(device_category) = 'monitoring devices' THEN 'iot'
-    WHEN lower(device_category) = 'camera' THEN 'security'
-    ELSE device_category 
+  CASE 
+    WHEN lower(device_category) = 'monitoring devices' THEN '4'
+    WHEN lower(device_category) = 'camera' THEN '5'
+    WHEN lower(device_category) = 'printer' THEN '6'
+    ELSE '2' 
   END AS role_id,
   1 as mac_auth,
-  0 as do_expire
+  0 as do_expire,
+  'Device imported from Endpoint: '  || mac_address || ' (' || hostname || ')' as notes
   FROM public.tips_endpoints_view 
   WHERE device_category LIKE 'Printer'
+    AND mac_address ~ '^[A-Fa-f0-9:\-]*$'
 ) TO stdout
 WITH CSV HEADER;
